8cc07fe545977f45e1a79d1f9eccb48e9e837f69
MasterX01 <aakashsaxena14@gmail.com>
2021-04-06 07:24:13 +0530

    [Akash] ADD. test case for data range with weekday and weekend

--- src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservation/HotelReservationTest.java
@@ -6,6 +6,8 @@ import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+
 public class HotelReservationTest {
     HotelReservation hotelreservation = new HotelReservation();
 
@@ -39,6 +41,17 @@ public class HotelReservationTest {
         Hotel hotel = new Hotel("Bridgewood", 220, 150);
         Assertions.assertEquals("Bridgewood", hotel.hotelName);
         Assertions.assertEquals(150, hotel.regularWeekEndRate);
+    }
+
+    @Test
+    public void given3Hotels_whenGivenDatesIncludingWeekends_ShouldReturnCheapestHotel() throws ParseException {
+        Hotel hotel1 = new Hotel("Lakewood", 110, 90);
+        Hotel hotel2 = new Hotel("Bridgewood", 150, 50);
+        Hotel hotel3 = new Hotel("Ridgewood", 220, 150);
+        hotelreservation.add(hotel1, hotel2, hotel3);
+        Hotel cheapHotel = hotelreservation.findCheapestHotel("11Sep2020", "12Sep2020");
+        Assertions.assertEquals("Lakewood", cheapHotel.hotelName);
+
     }
 
 }
