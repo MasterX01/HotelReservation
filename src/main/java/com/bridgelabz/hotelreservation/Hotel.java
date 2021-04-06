a7589848c32b14f996156e6822027daa4256bfd0
MasterX01 <aakashsaxena14@gmail.com>
2021-04-06 07:32:24 +0530

    [Akash] REACTOR. price calculations changed as per changing weekday

--- src/main/java/com/bridgelabz/hotelreservation/Hotel.java
+++ src/main/java/com/bridgelabz/hotelreservation/Hotel.java
@@ -1,5 +1,10 @@
 package com.bridgelabz.hotelreservation;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Hotel {
     public String hotelName;
     public int regularWeekDayRate;
@@ -18,12 +23,24 @@ public class Hotel {
         return str;
     }
 
-    // method to calculate price
-    public int calculatePrice(String...days) {
+    // method to get day-weekday/weekend
+    public static String getDayOfWeek(String date) throws ParseException {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMMyyyy");
+        Date actualDate = dateFormat.parse(date);
+        DateFormat dayFormat = new SimpleDateFormat("EE");
+        String day = dayFormat.format(actualDate);
+        return day;
+    }
+
+    public int calculatePrice(String... days) throws ParseException {
         int totalAmount = 0;
-        int day=0;
-        while(day<days.length) {
-            totalAmount += this.regularWeekDayRate;
+        for (int day = 0; day < days.length; day++) {
+            String whichDay = getDayOfWeek(days[day]);
+            if (whichDay == "Sat" || whichDay == "Sun")
+                totalAmount += this.regularWeekEndRate;
+            else
+                totalAmount += this.regularWeekDayRate;
             day++;
         }
         return totalAmount;
